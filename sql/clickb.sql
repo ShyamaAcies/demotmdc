f8d1c2d0bc20b9d39ce30efb46d163ecd58bbce6
ShyamaAcies <shyamacharan.urmalia@aciesglobal.com>
2025-06-03 15:29:33 +0530

    sql and tables folder

---
+++ sql/clickb.sql
@@ -0,0 +1,14 @@
+SELECT
+   ds,
+   location_id,
+   geo_id,
+   location_name,
+   geo_name,
+   booking_id,
+   advertiser_id,
+   campaign_id,
+   user_id,
+   impression_count,
+   interaction_count
+FROM
+   lakehouse.nivedhaa.click_shyamahb 
---
+++ sql/loc.sql
@@ -0,0 +1,17 @@
+SELECT
+    ds,
+    user_id,
+    location_id,
+    geo_id,
+    location_name,
+    geo_name,
+    booking_id,
+    advertiser_id,
+    campaign_id,
+    booking_amount,
+    currency,
+    booking_status,
+    impression_count,
+    interaction_count,
+    pageview_count
+FROM lakehouse.nivedhaa.user_shyamaloc    
